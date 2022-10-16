fe735aa4bc00e89a4e4c19866bffa318e23be12f
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-16 20:58:49 +0530

    view all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule,Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { ViewAllTrainComponent } from './view-all-train/view-all-train.component';
+const appRoutes:Routes=[
+  {
+    path:"",component:ViewAllTrainComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewAllTrainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
